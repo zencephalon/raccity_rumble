c25f4acbefaee269e20bca86a23d18ef651741ca
Matthew Bunday <zenceph@gmail.com>
2014-11-24 09:37:32 -0500

    Handle collision detection and creating an arena.

--- public/js/raccoon.js
+++ public/js/raccoon.js
@@ -1,12 +1,23 @@
-raccoon = new Raccoon();
+function Arena(height, width) {
+  this.height = height;
+  this.width = width;
+  this.$element = $('<div id="arena"></div>');
+  this.$element.css('height', height);
+  this.$element.css('width', width);
+  $('body').append(this.$element);
+}
 
-function Raccoon(x, y) {
+
+function Raccoon(x, y, arena) {
+  this.arena = arena;
   this.x = x;
   this.y = y;
   this.dir = "none";
-  this.sprite = $('<div id="good_raccoon"></div>');
-  this.sprite.css('position', 'relative');
-  $('#arena').append(this.sprite);
+  this.$sprite = $('<div id="good_raccoon"></div>');
+  this.$sprite.css('position', 'relative');
+  $('#arena').append(this.$sprite);
+  this.height = this.$sprite.height();
+  this.width = this.$sprite.width();
 }
 
 Raccoon.prototype.setDirection = function(direction) {
@@ -14,11 +25,14 @@ Raccoon.prototype.setDirection = function(direction) {
 }
 
 Raccoon.prototype.updateSpritePosition = function() {
-  this.sprite.css('top', this.y);
-  this.sprite.css('left', this.x);
+  this.$sprite.css('top', this.y - this.height / 2);
+  this.$sprite.css('left', this.x - this.width / 2);
 }
 
 Raccoon.prototype.move = function() {
+  var old_x = this.x;
+  var old_y = this.y;
+
   switch (this.dir) {
     case 'up':
       this.y -= 5;
@@ -33,11 +47,19 @@ Raccoon.prototype.move = function() {
       this.x += 5;
       break;
   }
+  if (((this.y - this.height / 2) < 0) || 
+      ((this.y + this.height / 2) > this.arena.height) ||
+      ((this.x - this.width / 2) < 0) ||
+      ((this.x + this.width / 2) > this.arena.width)) {
+    this.x = old_x;
+    this.y = old_y;
+  }
   this.updateSpritePosition();
 }
 
 $(document).ready(function() {
-  raccoon = new Raccoon(10, 5);
+  arena = new Arena(600, 1200);
+  raccoon = new Raccoon(300, 300, arena);
 
   ['down', 'up', 'left', 'right', 'space'].forEach(function(dir) {
     Mousetrap.bind(dir, function(event) {
