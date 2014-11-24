30638e7f8f5ee4f39672f95843d448ef69651062
Matthew Bunday <zenceph@gmail.com>
2014-11-24 09:23:54 -0500

    Handle raccoon movement.

--- public/js/raccoon.js
+++ public/js/raccoon.js
@@ -1,9 +1,16 @@
+raccoon = new Raccoon();
+
 function Raccoon(x, y) {
   this.x = x;
   this.y = y;
+  this.dir = "none";
   this.sprite = $('<div id="good_raccoon"></div>');
   this.sprite.css('position', 'relative');
   $('#arena').append(this.sprite);
+}
+
+Raccoon.prototype.setDirection = function(direction) {
+  this.dir = direction;
 }
 
 Raccoon.prototype.updateSpritePosition = function() {
@@ -12,9 +19,31 @@ Raccoon.prototype.updateSpritePosition = function() {
 }
 
 Raccoon.prototype.move = function() {
-  this.x += 5;
+  switch (this.dir) {
+    case 'up':
+      this.y -= 5;
+      break;
+    case 'down':
+      this.y += 5;
+      break;
+    case 'left':
+      this.x -= 5;
+      break;
+    case 'right':
+      this.x += 5;
+      break;
+  }
+  this.updateSpritePosition();
 }
 
 $(document).ready(function() {
   raccoon = new Raccoon(10, 5);
+
+  ['down', 'up', 'left', 'right', 'space'].forEach(function(dir) {
+    Mousetrap.bind(dir, function(event) {
+      raccoon.setDirection(dir);
+    })
+  });
+
+  setInterval(function() { raccoon.move() }, 20);
 })
