67d1d20c17c71d1a22de62e59161ad0a3941bd60
Matthew Bunday <zenceph@gmail.com>
2014-11-25 10:18:05 -0500

    Add enemy model.

---
+++ public/js/enemy.js
@@ -0,0 +1,13 @@
+function Enemy(arena) {
+  this.arena = arena;
+  this.dir = "left";
+  this.$sprite = $('<div id="enemy_raccoon"></div>');
+  $('#arena').append(this.$sprite);
+  this.height = this.$sprite.height();
+  this.width = this.$sprite.width();
+  this.x = this.arena.width - this.width / 2;
+  this.y = (this.arena.height - this.height) * Math.random() + this.height / 2;
+}
+
+Enemy.prototype.move = Raccoon.prototype.move;
+Enemy.prototype.updateSpritePosition = Raccoon.prototype.updateSpritePosition;
